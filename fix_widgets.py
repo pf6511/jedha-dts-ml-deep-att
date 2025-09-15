5ec84cd38f23ed5cc41feab4be6fb95172a2e7b2
pf6511 <pfelz@free.fr>
2025-09-15 11:06:28 +0200

    fix widgets

---
+++ fix_widgets.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+notebook_path = "DeepLearning_ATT_SpamDetector.ipynb"  # Path to your notebook
+
+with open(notebook_path, "r", encoding="utf-8") as f:
+    nb = json.load(f)
+
+# Remove the widgets metadata entirely
+if "widgets" in nb.get("metadata", {}):
+    del nb["metadata"]["widgets"]
+
+with open(notebook_path, "w", encoding="utf-8") as f:
+    json.dump(nb, f, indent=1)
+
+print(f"Removed widgets metadata from {notebook_path}")
